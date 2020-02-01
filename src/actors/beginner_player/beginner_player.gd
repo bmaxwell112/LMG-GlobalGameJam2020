19928fbbffce98f65d650c4b52426a80d1811e4e
Josh MacLachlan <josh.t.maclachlan@gmail.com>
2020-02-01 14:45:08 -0500

    Add Stamina for player, some work on robot

--- src/actors/beginner_player/beginner_player.gd
+++ src/actors/beginner_player/beginner_player.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 var walk_speed: int = 300
 var run_speed: int = 500
 var velocity: Vector2 = Vector2()
+var stamina: float = 100.0
 
 func _physics_process(delta: float) -> void:
   velocity = _get_movement_velocity()
@@ -13,7 +14,14 @@ func _get_movement_velocity() -> Vector2:
     (float(Input.is_action_pressed("move_right")) - float(Input.is_action_pressed("move_left"))),
     (float(Input.is_action_pressed("move_down")) - float(Input.is_action_pressed("move_up")))
   ).normalized()
+
   if Input.is_action_pressed("run"):
-    return movement_velocity * run_speed
+    if stamina > 0.0:
+      stamina -= 0.25
+      return movement_velocity * run_speed
+    else:
+      return movement_velocity * walk_speed
   else:
+    if stamina < 100.0:
+      stamina += 0.25
     return movement_velocity * walk_speed
